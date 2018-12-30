894e46cb953edd57e1959906d53bbdef1e10afee
Chris Aholt <35589544+chrisaholt@users.noreply.github.com>
2018-12-29 21:36:50 -0800

    Write name with arbitrary names.

--- examples/WriteName.js
+++ examples/WriteName.js
@@ -1,20 +1,18 @@
-async function ShowName(delayTime)
+async function ShowName(name, delayTime)
 {
     var letterColor = {r:0, g:0, b:255};
-    setMatrixCharacter('N', letterColor);
-    await delay (delayTime);
-    setMatrixCharacter('A', letterColor);
-    await delay (delayTime);
-    setMatrixCharacter('M', letterColor);
-    await delay (delayTime);
-    setMatrixCharacter('E', letterColor);
-    await delay (delayTime);
+
+    for (var iStr = 0; iStr < name.length; ++iStr)
+    {
+        setMatrixCharacter(name.charAt(iStr), letterColor);
+        await delay (delayTime);
+    }
 }
 
 async function startProgram()
 {
     while (true)
     {
-        await ShowName(1);
+        await ShowName("Program", 1);
     }
 }
